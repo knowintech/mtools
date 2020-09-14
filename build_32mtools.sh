21ade9d25e2a94fc13e96e077dd9fc45c3210b83
houyingli <houyingli@knowin.com>
2020-09-14 18:17:08 +0800

    modify cross build script

--- build_32mtools.sh
+++ build_32mtools.sh
@@ -1,17 +1,8 @@
 
-#export PATH=$(pwd)/../standalone_toolchains32/bin/:$PATH
-export PATH=$ANDROID_NDK/android-ndk-r18b/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/:$PATH
-echo $ANDROID_NDK
-echo $PATH
+export PATH=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/:$PATH
 
-DEST_GCC=aarch64-linux-android-gcc
-DEST_AR=aarch64-linux-android-ar
-DEST_RANLIB=aarch64-linux-android-ranlib
-DEST_STRIP=aarch64-linux-android-strip
-
-DEST_HOST=aarch64-linux-android
-DEST_TARGET=aarch64-linux
+DEST_GCC=armv7a-linux-androideabi29-clang
 
 make clean
-make CC=$DEST_GCC CXX=$DEST_CXX 
+make CC=$DEST_GCC 
 
