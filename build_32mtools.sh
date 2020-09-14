a3300cd09f2273a80eab53f8c13319c9484b7a97
houyingli <houyingli@knowin.com>
2020-09-14 19:32:44 +0800

    exit shell when env ANDROID_NDK  no set

--- build_32mtools.sh
+++ build_32mtools.sh
@@ -1,4 +1,9 @@
 
+if [ ! -d "$ANDROID_NDK" ]; then
+	  echo "please set env var ANDROID_NDK for ndk directory"
+	  exit 1
+fi
+
 export PATH=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/:$PATH
 
 DEST_GCC=armv7a-linux-androideabi29-clang
