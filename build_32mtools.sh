a631e55318adfac4645ea1242aa7a9847df9bc80
ouyang <jxfengzi@gmail.com>
2020-09-14 15:57:03 +0800

    update :

--- build_32mtools.sh
+++ build_32mtools.sh
@@ -1,13 +1,16 @@
 
-export PATH=$(pwd)/../standalone_toolchains32/bin/:$PATH
+#export PATH=$(pwd)/../standalone_toolchains32/bin/:$PATH
+export PATH=$ANDROID_NDK/android-ndk-r18b/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/:$PATH
+echo $ANDROID_NDK
+echo $PATH
 
-DEST_GCC=arm-linux-androideabi-gcc
-DEST_AR=arm-linux-androideabi-ar
-DEST_RANLIB=arm-linux-androideabi-ranlib
-DEST_STRIP=arm-linux-androideabi-strip
+DEST_GCC=aarch64-linux-android-gcc
+DEST_AR=aarch64-linux-android-ar
+DEST_RANLIB=aarch64-linux-android-ranlib
+DEST_STRIP=aarch64-linux-android-strip
 
-DEST_HOST=arm-linux-androideabi
-DEST_TARGET=arm-linux
+DEST_HOST=aarch64-linux-android
+DEST_TARGET=aarch64-linux
 
 make clean
 make CC=$DEST_GCC CXX=$DEST_CXX 
